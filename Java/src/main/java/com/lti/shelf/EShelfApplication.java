95e05b0b66b1134b1c17245e4769fd9bde2eea9b
JohnAbhishikth <kjabhishikth@gmail.com>
2021-11-19 20:39:34 +0530

    Java Project Creation

---
+++ Java/src/main/java/com/lti/shelf/EShelfApplication.java
@@ -0,0 +1,13 @@
+package com.lti.shelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EShelfApplication.class, args);
+	}
+
+}
---
+++ Java/src/test/java/com/lti/shelf/EShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.shelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
