128d9a194c7e0eb799d4bb2e7a0caf5cd74a8d3e
MaheshKolluri965 <maheshkolluri8179@gmail.com>
2022-01-11 13:42:28 +0530

    shoppingcartservice updated

--- Java/src/main/java/com/lti/shelf/service/ShoppingCartItemServiceImpl.java
+++ Java/src/main/java/com/lti/shelf/service/ShoppingCartItemServiceImpl.java
@@ -1,37 +1,68 @@
 package com.lti.shelf.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.lti.shelf.dto.ShoppingCartDTO;
 import com.lti.shelf.entity.ShoppingCartItem;
+import com.lti.shelf.entity.ShoppingCartItemPK;
 import com.lti.shelf.exception.EShelfException;
 import com.lti.shelf.repository.ShoppingCartItemRepository;
 
-
-
 @Service
-@Transactional
 public class ShoppingCartItemServiceImpl implements ShoppingCartItemService {
 
-	
 	@Autowired
-	ShoppingCartItemRepository jpaRepo;
-	
+	ShoppingCartItemRepository shoppingCartRepository;
 
 	@Override
 	public void addItemToCart(ShoppingCartDTO shoppingCartDto) throws EShelfException {
-		// TODO Auto-generated method stub
-		
+
+		try {
+			if (shoppingCartDto == null) {
+				throw new EShelfException("ShoppingCart Item Cant be null");
+			}
+
+			ShoppingCartItemPK cartItemPK = new ShoppingCartItemPK(shoppingCartDto.getInventoryId(),
+					shoppingCartDto.getUserId());
+			ShoppingCartItem shoppingCartItem = new ShoppingCartItem(cartItemPK, shoppingCartDto.getPrice(),
+					shoppingCartDto.getQuantity());
+
+			shoppingCartRepository.save(shoppingCartItem);
+		} catch (Exception e) {
+			throw new EShelfException("Unable to add to cart");
+		}
 	}
 
 	@Override
 	public List<ShoppingCartDTO> getCartByUserId(String userId) throws EShelfException {
-		// TODO Auto-generated method stub
-		return null;
+		if (userId == null || userId.length() == 0) {
+			throw new EShelfException("Enter Valid User Id");
+		}
+
+		List<ShoppingCartItem> allShoppingCartItemsByUserId = shoppingCartRepository
+				.getAllShoppingCartItemsByUserId(userId);
+		if (allShoppingCartItemsByUserId.isEmpty())
+			throw new EShelfException("No details");
+
+		List<ShoppingCartDTO> shoppingCartDTOList = new ArrayList<>();
+		try {
+			for (ShoppingCartItem shoppingCartItem : allShoppingCartItemsByUserId) {
+				ShoppingCartDTO shoppingCartDTO = new ShoppingCartDTO();
+				shoppingCartDTO.setInventoryId(shoppingCartItem.getBook().getInventoryId());
+				shoppingCartDTO.setPrice(shoppingCartItem.getPrice());
+				shoppingCartDTO.setQuantity(shoppingCartItem.getQuantity());
+				shoppingCartDTO.setUserId(userId);
+				shoppingCartDTOList.add(shoppingCartDTO);
+			}
+			return shoppingCartDTOList;
+		} catch (Exception e) {
+			throw new EShelfException(e.getMessage());
+		}
+
 	}
 
 	@Override
@@ -42,53 +73,43 @@ public class ShoppingCartItemServiceImpl implements ShoppingCartItemService {
 
 	@Override
 	public boolean deleteOneShoppingCartItem(String userId, String inventoryId) throws EShelfException {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			ShoppingCartItemPK shoppingCartItemPK = new ShoppingCartItemPK(inventoryId, userId);
+			shoppingCartRepository.deleteById(shoppingCartItemPK);
+			return true;
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 
 	@Override
 	public boolean deleteAllShoppingCartItems(String userId) throws EShelfException {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			List<ShoppingCartDTO> cartByUserId = this.getCartByUserId(userId);
+			for (ShoppingCartDTO dto : cartByUserId) {
+				ShoppingCartItemPK shoppingCartItemPK = new ShoppingCartItemPK(dto.getInventoryId(), dto.getUserId());
+				shoppingCartRepository.deleteById(shoppingCartItemPK);
+			}
+			return true;
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 
-	
 	/*
-	@Override
-	public void addShoppingCI(ShoppingCartItem sci) {
-		if(sci!=null) 
-		{
-			try {	
-				jpaRepo.save(sci);	
-			}catch(Exception e) {
-				System.out.print(e);
-			}
-		}
-		
-	}
-
-	@Override
-	public List<ShoppingCartItem> getShoppingCI() {
-		return jpaRepo.findAll();
-	}
-
-	@Override
-	public boolean updateShoppingCI(ShoppingCartItem sci) {
-		if(sci!=null) 
-		{
-			try {	
-				jpaRepo.save(sci);	
-			}catch(Exception e) {
-				System.out.print(e);
-			}
-		}
-		return true;
-	}
-
-	@Override
-	public boolean deleteShoppingCI(ShoppingCartItem sci) {
-		jpaRepo.delete(sci);
-		return true;
-	}
-*/
+	 * @Override public void addShoppingCI(ShoppingCartItem sci) { if(sci!=null) {
+	 * try { jpaRepo.save(sci); }catch(Exception e) { System.out.print(e); } }
+	 * 
+	 * }
+	 * 
+	 * @Override public List<ShoppingCartItem> getShoppingCI() { return
+	 * jpaRepo.findAll(); }
+	 * 
+	 * @Override public boolean updateShoppingCI(ShoppingCartItem sci) {
+	 * if(sci!=null) { try { jpaRepo.save(sci); }catch(Exception e) {
+	 * System.out.print(e); } } return true; }
+	 * 
+	 * @Override public boolean deleteShoppingCI(ShoppingCartItem sci) {
+	 * jpaRepo.delete(sci); return true; }
+	 */
 }
